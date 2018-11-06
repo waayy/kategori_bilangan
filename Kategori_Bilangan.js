1d6ed267a007f8797ddd4ecac474f3c9c728f840
waayy <44268485+waayy@users.noreply.github.com>
2018-11-06 15:17:47 +0700

    Update Kategori_Bilangan.js

--- Kategori_Bilangan.js
+++ Kategori_Bilangan.js
@@ -1,83 +1,83 @@
 
-var i = 0; 
-var kelompok = []; 
-let hasil = []; 
+// var i = 0; 
+// var kelompok = []; 
+// let hasil = []; 
 
 
-let bilangan = (x) =>{
+// let bilangan = (x) =>{
 
-// jika kelompok bilangan bulat
+// // jika kelompok bilangan bulat
 
-        if(x >= 0 || x < 0){
-            hasil.push("bulat");
-                i++;
-        }
+//         if(x >= 0 || x < 0){
+//             hasil.push("bulat");
+//                 i++;
+//         }
            
-//jika kelompok bilangan cacah
+// //jika kelompok bilangan cacah
 
-        if (i == 1){ 
-            if(x >= 0){ 
-                hasil.push("cacah");
-                    i++;
-            }
-        }
+//         if (i == 1){ 
+//             if(x >= 0){ 
+//                 hasil.push("cacah");
+//                     i++;
+//             }
+//         }
 
-//jika kelompok bilangan negatif
+// //jika kelompok bilangan negatif
 
-        if (i == 1){ 
-            if(x < 0){ 
-                hasil.push("negatif");
-                    i++;
-            }
-        }    
+//         if (i == 1){ 
+//             if(x < 0){ 
+//                 hasil.push("negatif");
+//                     i++;
+//             }
+//         }    
 
-//jika kelompok bilangan nol
+// //jika kelompok bilangan nol
         
-        if(x == 0){ 
-            hasil.push("nol");
-        }
+//         if(x == 0){ 
+//             hasil.push("nol");
+//         }
 
-//jika kelompok bilangan asli
+// //jika kelompok bilangan asli
         
-        if(x > 0){
-            hasil.push('asli')
-        }
+//         if(x > 0){
+//             hasil.push('asli')
+//         }
 
-//jika kelompok bilangan ganjil
+// //jika kelompok bilangan ganjil
         
-        if(x % 2 != 0){
-            hasil.push('ganjil')
-        }
+//         if(x % 2 != 0){
+//             hasil.push('ganjil')
+//         }
 
-//jika kelompok bilangan genap
+// //jika kelompok bilangan genap
             
-            if(x % 2 == 0){
-                hasil.push('genap')
-            }
+//             if(x % 2 == 0){
+//                 hasil.push('genap')
+//             }
 
-//jika kelompok bilangan prima 
+// //jika kelompok bilangan prima 
     
-        for(i=2; i<=x; i++){
-            if(i>=2){
-            var prima = true
-            }
-        }
-        for(j=2; j<i; j++){
-            if(i%j==0){
-            var prima = false
-            }        
-        }
+//         for(i=2; i<=x; i++){
+//             if(i>=2){
+//             var prima = true
+//             }
+//         }
+//         for(j=2; j<i; j++){
+//             if(i%j==0){
+//             var prima = false
+//             }        
+//         }
 
-            if(prima == true){
-                hasil.push('prima')
-            }
+//             if(prima == true){
+//                 hasil.push('prima')
+//             }
             
-            if(prima == false){
-                hasil.push('prima')
-            }
+//             if(prima == false){
+//                 hasil.push('prima')
+//             }
 
-return hasil;
-hasil = [];
+// return hasil;
+// hasil = [];
 
 }
 // console.log(bilangan(0))
