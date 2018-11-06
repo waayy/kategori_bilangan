9d34de61f91f0347e588e5a4a90261b958b7e308
waayy <44268485+waayy@users.noreply.github.com>
2018-11-06 15:16:39 +0700

    Add files via upload
    kategori Bilangan

---
+++ Kategori_Bilangan.js
@@ -0,0 +1,91 @@
+
+var i = 0; 
+var kelompok = []; 
+let hasil = []; 
+
+
+let bilangan = (x) =>{
+
+// jika kelompok bilangan bulat
+
+        if(x >= 0 || x < 0){
+            hasil.push("bulat");
+                i++;
+        }
+           
+//jika kelompok bilangan cacah
+
+        if (i == 1){ 
+            if(x >= 0){ 
+                hasil.push("cacah");
+                    i++;
+            }
+        }
+
+//jika kelompok bilangan negatif
+
+        if (i == 1){ 
+            if(x < 0){ 
+                hasil.push("negatif");
+                    i++;
+            }
+        }    
+
+//jika kelompok bilangan nol
+        
+        if(x == 0){ 
+            hasil.push("nol");
+        }
+
+//jika kelompok bilangan asli
+        
+        if(x > 0){
+            hasil.push('asli')
+        }
+
+//jika kelompok bilangan ganjil
+        
+        if(x % 2 != 0){
+            hasil.push('ganjil')
+        }
+
+//jika kelompok bilangan genap
+            
+            if(x % 2 == 0){
+                hasil.push('genap')
+            }
+
+//jika kelompok bilangan prima 
+    
+        for(i=2; i<=x; i++){
+            if(i>=2){
+            var prima = true
+            }
+        }
+        for(j=2; j<i; j++){
+            if(i%j==0){
+            var prima = false
+            }        
+        }
+
+            if(prima == true){
+                hasil.push('prima')
+            }
+            
+            if(prima == false){
+                hasil.push('prima')
+            }
+
+return hasil;
+hasil = [];
+
+}
+// console.log(bilangan(0))
+console.log(bilangan(11))
+// console.log(bilangan(-77)) 
+// console.log(bilangan(8))
+
+
+
+
+
